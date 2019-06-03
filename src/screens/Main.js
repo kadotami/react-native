f621a4c04ac046bd85f2f4d67a389156305c7ea1
kadotami <mondamin.kqfb@gmail.com>
2019-06-04 00:14:30 +0900

    modify

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,27 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, FlatList, Image} from 'react-native';
 import { Container, Header, Left, Body, Right, Button, Icon, Title } from 'native-base';
-import { StackNavigator } from 'react-navigation';
-// const instructions = Platform.select({
-//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-//   android:
-//     'Double tap R on your keyboard to reload,\n' +
-//     'Shake or press menu button for dev menu',
-// });
-
-
 
 type Props = {};
-export default class App extends Component<Props> {
-
+export default class Main extends Component<Props> {
   constructor(props) {
     super(props);
     this.state = {
